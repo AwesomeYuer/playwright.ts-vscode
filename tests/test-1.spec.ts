29fc18c18b49651b7a1d660ac3b993d066d1990f
AwesomeYuer <AwesomeYuer@Microshaoft.com>
2023-02-23 17:53:33 +0800

    1

---
+++ tests/test-1.spec.ts
@@ -0,0 +1,71 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+
+  test.slow();
+  await page.goto('https://awesomeyuer.github.io/JsonStructuresDiffAssistantToolsOnline/');
+  await page.getByRole('button', { name: 'Next →' }).click();
+  await new Promise(r => setTimeout(r, 1000));
+  await page.getByText('Loading ... press Esc for cancel waiting Json Structures Diff Assistant Tools On').press('ArrowRight');
+  await new Promise(r => setTimeout(r, 1000));
+  await page.getByText('Loading ... press Esc for cancel waiting Json Structures Diff Assistant Tools On').press('ArrowRight');
+  await new Promise(r => setTimeout(r, 1000));
+  await page.getByText('Loading ... press Esc for cancel waiting Json Structures Diff Assistant Tools On').press('ArrowRight');
+  await new Promise(r => setTimeout(r, 1000));  
+  await page.getByText('Loading ... press Esc for cancel waiting Json Structures Diff Assistant Tools On').press('ArrowRight');
+  await new Promise(r => setTimeout(r, 1000));
+  await page.getByText('Loading ... press Esc for cancel waiting Json Structures Diff Assistant Tools On').press('ArrowRight');
+  await new Promise(r => setTimeout(r, 1000));
+  await page.getByText('Loading ... press Esc for cancel waiting Json Structures Diff Assistant Tools On').press('ArrowRight');
+  await new Promise(r => setTimeout(r, 1000));
+  await page.getByText('Loading ... press Esc for cancel waiting Json Structures Diff Assistant Tools On').press('ArrowRight');
+  await new Promise(r => setTimeout(r, 1000));
+  await page.getByText('Loading ... press Esc for cancel waiting Json Structures Diff Assistant Tools On').press('ArrowRight');
+  await new Promise(r => setTimeout(r, 1000));
+  await page.getByText('Loading ... press Esc for cancel waiting Json Structures Diff Assistant Tools On').press('ArrowRight');
+  await new Promise(r => setTimeout(r, 1000));
+  await page.getByText('Loading ... press Esc for cancel waiting Json Structures Diff Assistant Tools On').press('ArrowRight');
+  await new Promise(r => setTimeout(r, 1000));
+  await page.getByText('Loading ... press Esc for cancel waiting Json Structures Diff Assistant Tools On').press('ArrowRight');
+  await new Promise(r => setTimeout(r, 1000));
+  await page.getByText('Loading ... press Esc for cancel waiting Json Structures Diff Assistant Tools On').press('ArrowRight');
+  await new Promise(r => setTimeout(r, 1000));
+  await page.getByText('Loading ... press Esc for cancel waiting Json Structures Diff Assistant Tools On').press('ArrowRight');
+  await new Promise(r => setTimeout(r, 1000));
+  await page.getByText('Loading ... press Esc for cancel waiting Json Structures Diff Assistant Tools On').press('ArrowRight');
+  await new Promise(r => setTimeout(r, 1000));
+  await page.getByText('Loading ... press Esc for cancel waiting Json Structures Diff Assistant Tools On').press('ArrowRight');
+  await new Promise(r => setTimeout(r, 1000));
+  await page.getByText('Loading ... press Esc for cancel waiting Json Structures Diff Assistant Tools On').press('ArrowRight');
+  await new Promise(r => setTimeout(r, 1000));
+  await page.getByText('Loading ... press Esc for cancel waiting Json Structures Diff Assistant Tools On').press('ArrowRight');
+  await new Promise(r => setTimeout(r, 1000));
+  await page.getByText('Loading ... press Esc for cancel waiting Json Structures Diff Assistant Tools On').press('ArrowRight');
+  await new Promise(r => setTimeout(r, 1000));
+  await page.getByText('Loading ... press Esc for cancel waiting Json Structures Diff Assistant Tools On').press('ArrowRight');
+  await new Promise(r => setTimeout(r, 1000));
+  await page.getByText('Loading ... press Esc for cancel waiting Json Structures Diff Assistant Tools On').press('ArrowLeft');
+  await new Promise(r => setTimeout(r, 1000));
+  await page.getByText('Loading ... press Esc for cancel waiting Json Structures Diff Assistant Tools On').press('ArrowLeft');
+  await new Promise(r => setTimeout(r, 1000));
+  await page.getByText('Loading ... press Esc for cancel waiting Json Structures Diff Assistant Tools On').press('ArrowLeft');
+  await new Promise(r => setTimeout(r, 1000));
+  await page.getByText('Loading ... press Esc for cancel waiting Json Structures Diff Assistant Tools On').press('Enter');
+  await new Promise(r => setTimeout(r, 1000));
+  await page.getByText('Loading ... press Esc for cancel waiting Json Structures Diff Assistant Tools On').press('Enter');
+  await new Promise(r => setTimeout(r, 1000));
+  await page.getByText('Loading ... press Esc for cancel waiting Json Structures Diff Assistant Tools On').press('Escape');
+  await new Promise(r => setTimeout(r, 1000));
+  await page.getByText('Loading ... press Esc for cancel waiting Json Structures Diff Assistant Tools On').press('Alt+s');
+  await new Promise(r => setTimeout(r, 1000));
+  await page.getByText('Loading ... press Esc for cancel waiting Json Structures Diff Assistant Tools On').press('Alt+z');
+
+  await page.getByText('Side (z)').click();
+  await page.getByLabel('Side (z)').press('Alt+o');
+  await page.getByLabel('Side (z)').press('Alt+i');
+
+  await page.keyboard.press("Alt+z");
+
+  await new Promise(r => setTimeout(r, 5000));
+
+});
